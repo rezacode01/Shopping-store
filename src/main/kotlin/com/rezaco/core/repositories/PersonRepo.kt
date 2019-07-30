957636d3a808ca88d9b17694a483f4f42f8d6135
Reza Ahmadi <rezacode01@gmail.com>
2019-07-30 15:01:25 +0430

    person repo added

---
+++ src/main/kotlin/com/rezaco/core/repositories/PersonRepo.kt
@@ -0,0 +1,6 @@
+package com.rezaco.core.repositories
+
+import com.rezaco.core.models.Person
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface PersonRepo : JpaRepository<Person, Long>
